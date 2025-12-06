e519e14853a1b643a076de108f658fcbd8e771b7
James Oswald <jamesoswald111@gmail.com>
2025-12-05 19:53:58 -0500

    YY-board

--- level_gen/__init__.py
+++ level_gen/__init__.py
@@ -3,6 +3,6 @@ from .YY_tiny_board_bridge import generate_YY_bridge_problem
 from .maze import generate_maze
 
 GENERATORS = {
-    #"YY-tiny-board-bridge": generate_YY_bridge_problem,
-    "maze": generate_maze,
+    "YY-tiny-board-bridge": generate_YY_bridge_problem,
+    #"maze": generate_maze,
 }
